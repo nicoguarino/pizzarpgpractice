0ad71e3a8730e097b30c0a5aaa6d3549c5c75c23
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-03-15 16:26:32 -0700

    got shadows to render again for new sprite objects

--- Sprite.js
+++ Sprite.js
@@ -8,6 +8,17 @@ class Sprite {
       this.isLoaded = true;
     }
 
+    //Shadow
+    this.shadow = new Image();
+    this.useShadow = true; // config.useShadow || false
+    if (this.useShadow) {
+      this.shadow.src = '/images/characters/shadow.png';
+    }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    }
+  
+
     // Configure Animation & Initial State
     this.animations = config.animations || {
       idleDown: [
@@ -28,6 +39,8 @@ class Sprite {
   draw(ctx) {
     const x = this.gameObject.x * 16 - 8;
     const y = this.gameObject.y * 16 - 16;
+
+    this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
 
     this.isLoaded && ctx.drawImage(this.image,
       0, 0,
