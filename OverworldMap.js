6adde3c16f1b59e3f44da3561c09d7e73d16ca15
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-03-20 14:10:12 -0700

    added OverworldMap.js, creating a lower and upper image to use multiple maps

---
+++ OverworldMap.js
@@ -0,0 +1,12 @@
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+
+  }
+}
