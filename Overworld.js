a417afa9702e6f73a6a7a3c13e3224e85147ae2a
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-03-20 14:24:51 -0700

    getting rid of hard code writtem in Overworld.js

--- Overworld.js
+++ Overworld.js
@@ -19,31 +19,6 @@ class Overworld {
 
     this.startGameLoop();
 
-    const image = new Image();
-
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0)
-    };
-    image.src = '/images/maps/DemoLower.png';
-
-    // Place some game objects!
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    })
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: '/images/characters/people/npc1.png'
-    })
-
-    setTimeout(() => {
-
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
-
   }
 
 }
