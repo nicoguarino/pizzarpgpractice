cf34ebf466b217fc996febe88ffeac5ab5875888
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-03-22 07:27:36 -0700

    created file DirectionInput.js, created event listener to capture key strokes. set directional movement to wasd and arrow keys. got hero to move!

--- Overworld.js
+++ Overworld.js
@@ -19,7 +19,7 @@ class Overworld {
       //Draw game objects
       Object.values(this.map.gameObjects).forEach(object => {
         object.update({
-
+          arrow: this.directionInput.direction
         });
         object.sprite.draw(this.ctx);
       })
@@ -36,6 +36,10 @@ class Overworld {
 
   init() {
     this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
+
     this.startGameLoop();
 
   }
