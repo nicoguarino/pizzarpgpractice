0f96444d8ea9dd50578247dd69f8081cfd276713
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-09 13:46:28 -0700

    finished created update position function and refactoring update() to start to cehck cases for walking

--- Person.js
+++ Person.js
@@ -19,21 +19,36 @@ class Person extends GameObject {
       this.updatePosition();
     } else {
 
-    }
-    this.updateSprite(state);
+      //More cases for starting to walk will be here
+      //
+      //
 
-    if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
-      this.startBehavior(state, {
-        type: 'walk',
-        direction: state.arrow
-      })
+      //Case: We're keyboard ready and have an arrow pressed
+      if (this.isPlayerControlled && state.arrow) {
+        this.startBehavior(state, {
+          type: 'walk',
+          direction: state.arrow
+        })
+      }
+      this.updateSprite(state);
     }
+
   }
 
   startBehavior(state, behavior) {
+    // Set chara direction to whatever behavior has 
     this.direction = behavior.direction;
-    state.map.isSpaceTaken(this.x, this.y, this.direction);
-    this.movingProgressRemaining = 16;
+
+    if (behavior.type === 'walk') {
+
+      //Stop here if space is not free (true)
+      if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+        return;
+      }
+
+      //ready to walk
+      this.movingProgressRemaining = 16;
+    }
   }
 
   updatePosition() {
