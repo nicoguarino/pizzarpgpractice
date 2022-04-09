749b849d547eb05f5e0451dc920f5d8d8ed03c4e
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-09 14:01:06 -0700

    added mount method to GameObject class

--- GameObject.js
+++ GameObject.js
@@ -1,5 +1,6 @@
 class GameObject {
   constructor(config) {
+    this.isMounted = false;
     this.x = config.x || 0;
     this.y = config.y || 0;
     this.direction = config.direction || 'down';
@@ -8,6 +9,12 @@ class GameObject {
       src: config.src || '/images/characters/people/hero.png',
     });
   }
+
+  mount (map) {
+    this.isMounted = true;
+    Map.addMap(this.x, this.y);
+  }
+
   update() {
 
   }
