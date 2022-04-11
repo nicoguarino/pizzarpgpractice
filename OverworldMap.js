af0ab017cdcbb2a558cab1f048e35c9089897cbc
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-11 16:16:58 -0700

    got a cutscene to work

--- OverworldMap.js
+++ OverworldMap.js
@@ -9,7 +9,7 @@ class OverworldMap {
     this.upperImage = new Image();
     this.upperImage.src = config.upperSrc;
 
-    this.isCutscenePlaying = false;
+    this.isCutscenePlaying = true;
   }
 
   drawLowerImage(ctx, cameraPerson) {
@@ -38,6 +38,20 @@ class OverworldMap {
       //TODO: determine if this object should actually mount
       object.mount(this);
     })
+  }
+
+  async startCutscene(events) {
+    this.isCutscenePlaying = true;
+
+    for (let i = 0; i < events.length; i++) {
+      const eventHandler = new OverworldEvent({
+        event: events[i],
+        map: this,
+      })
+      await eventHandler.init();
+    }
+
+    this.isCutscenePlaying = false;
   }
 
   addWall(x, y) {
--- Person.js
+++ Person.js
@@ -24,7 +24,7 @@ class Person extends GameObject {
       //
 
       //Case: We're keyboard ready and have an arrow pressed
-      if (this.isPlayerControlled && state.arrow) {
+      if (!state.map.isCutscenePlaying && this.isPlayerControlled && state.arrow) {
         this.startBehavior(state, {
           type: 'walk',
           direction: state.arrow
