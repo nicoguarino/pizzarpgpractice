b57f0d8b7c20260cfb4221ce289ec85b4927a49a
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-11 15:07:44 -0700

    added notes for what doBehaviorEvent method is doing

--- GameObject.js
+++ GameObject.js
@@ -29,16 +29,29 @@ class GameObject {
   }
 
   async doBehaviorEvent(map) {
+
+    // Don't do anything if there is a more important cutscene or I don't have to config to do anything
+    //anyway.
+    if (map.isCutscenePlaying || this.behaviorLoop.length === 0) {
+      return;
+    }
+
+    // Setting up our even with relevant info
     let eventConfig = this.behaviorLoop[this.behaviorLoopIndex];
     eventConfig.who = this.id;
 
+    //Create  an event instrance out of our nexst event config
     const eventHandler = new OverworldEvent({ map, event: eventConfig });
     await eventHandler.init();
 
+    //Setting the next event to fire
     this.behaviorLoopIndex += 1;
     if (this.behaviorLoopIndex === this.behaviorLoop.length) {
       this.behaviorLoopIndex = 0;
     }
+
+    //Do it again!
+    this.doBehaviorEvent(map);
   }
 
 }
