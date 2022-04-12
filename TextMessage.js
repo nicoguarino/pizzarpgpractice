e418539d73ab99942aae43ac99f2a095d7bdc2a3
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-12 15:07:32 -0700

    created styles folder to hold many css files. renamed styles.css to global.css

--- TextMessage.js
+++ TextMessage.js
@@ -7,9 +7,19 @@ class TextMessage {
 
   createElement() {
 
+    //Create the element
+    this.element = document.createElement('div');
+    this.element.classList.add('TextMessage');
+
+    this.element.innerHTML = (`
+      <p class="TextMessage_p"> ${this.text}</p>
+      <button class= "TextMessage_button">Next</button>
+    `)
+
   }
 
   init() {
-    
+    this.createElement();
+    CredentialsContainer.appendChild(this.element)
   }
 }
