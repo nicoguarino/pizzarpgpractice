3973d3bae0419d034ee51b1fa96ff4363185bf30
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-04-12 15:00:49 -0700

    created Textmessage class with constructor, createElement and init methods that haven't been written yet

---
+++ TextMessage.js
@@ -0,0 +1,15 @@
+class TextMessage {
+  constructor(text, onComplete) {
+    this.text = text;
+    this.coComplete = onComplete;
+    this.element = null;
+  }
+
+  createElement() {
+
+  }
+
+  init() {
+    
+  }
+}
