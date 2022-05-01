4c2a34085adccef2f213fe9236af15075eb7d4fa
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-01 10:50:41 -0700

    finished populating init method for now

--- RevealingText.js
+++ RevealingText.js
@@ -11,7 +11,17 @@ class RevealingText {
   init() {
     let characters = [];
     this.text.split("").forEach(character => {
+
+      //Create each span, add t o element in DOM
       let span = document.createElement("span");
+      span.textContent = character;
+      this.element.appendChild(span);
+
+      // Add this psan to our interal state Array
+      characters.push({
+        span,
+        delayAfter: character === " " ? 0 : this.speed
+      })
     })
   }
 }
