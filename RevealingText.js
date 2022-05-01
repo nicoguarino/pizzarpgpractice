e465e1885a4259a715d0549961bff4028a946df8
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-01 11:02:42 -0700

    creating revealingOneCharacter method

--- RevealingText.js
+++ RevealingText.js
@@ -8,11 +8,15 @@ class RevealingText {
     this.isDone = false;
   }
 
+  revealOneCharacter(list) {
+    
+  }
+
   init() {
     let characters = [];
     this.text.split("").forEach(character => {
 
-      //Create each span, add t o element in DOM
+      //Create each span, add to element in DOM
       let span = document.createElement("span");
       span.textContent = character;
       this.element.appendChild(span);
@@ -23,5 +27,7 @@ class RevealingText {
         delayAfter: character === " " ? 0 : this.speed
       })
     })
+
+    this.revealOneCharacter(characters);
   }
 }
