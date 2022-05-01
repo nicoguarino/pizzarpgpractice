d2f6c05765d8d1039b5d8e3f7e214a78f5b201fb
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-01 11:32:18 -0700

    refactoring parts of TextMessage.js

--- TextMessage.js
+++ TextMessage.js
@@ -28,15 +28,19 @@ class TextMessage {
     })
 
     this.actionListener = new KeyPressListener("Enter", () => {
-      this.actionListener.unbind();
       this.done();
     })
 
   }
 
   done() {
-    this.element.remove();
-    this.onComplete();
+    if (this.revealingText.isDone) {
+      this.element.remove();
+      this.actionListener.unbind();
+      this.onComplete();
+    } else {
+      this.revealingText.warpToDone();
+    }
   }
 
   init(container) {
