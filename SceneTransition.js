c94ad070992238d897344811e5bfa45593823fb9
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-01 14:25:17 -0700

    created createElement and init methods within SceneTransition

--- SceneTransition.js
+++ SceneTransition.js
@@ -1,5 +1,14 @@
 class SceneTransition {
   constructor() {
+    this.element = null;
+  }
+  createElement() {
+    this.element = document.createElement("div");
+    this.element.classList.add("SceneTransition")
+  }
 
+  init(container) {
+    this.createElement();
+    container.appendChild(this.element);
   }
 }
