aa63214b7059c717fd3f2ead44f71f817f654771
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-02 16:16:37 -0700

    added enemy html

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -7,8 +7,13 @@ class Battle {
     this.element = document.createElement("div");
     this.element.classList.add("Battle");
     this.element.innerHTML = (`
-    <div class="Battle_hero:>
+    <div class="Battle_hero">
       <img src="${'/images/characters/people/hero.png'} alt="Hero" />
+    </div>
+    
+    <div class="Battle_enemy">
+      <img src="${'/images/characters/people/npc3.png'} alt="Enemy" />
+    </div>
     `)
   }
 
