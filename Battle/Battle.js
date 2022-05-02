f25caea159f4af775d5e56e51285c8a1df5a2a18
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-02 16:14:43 -0700

    started to poplulate html to hold hero in createElement

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -7,6 +7,8 @@ class Battle {
     this.element = document.createElement("div");
     this.element.classList.add("Battle");
     this.element.innerHTML = (`
+    <div class="Battle_hero:>
+      <img src="${'/images/characters/people/hero.png'} alt="Hero" />
     `)
   }
 
