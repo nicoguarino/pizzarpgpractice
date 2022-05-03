725e65fc123b6047f703d1f4c47fa12a46e6fdaf
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-03 14:28:03 -0700

    found bug that didn't render the png images of the hero and Enemy

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -8,11 +8,11 @@ class Battle {
     this.element.classList.add("Battle");
     this.element.innerHTML = (`
     <div class="Battle_hero">
-      <img src="${'/images/characters/people/hero.png'} alt="Hero" />
+      <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
     </div>
 
     <div class="Battle_enemy">
-      <img src="${'/images/characters/people/npc3.png'} alt="Enemy" />
+      <img src=${'/images/characters/people/npc3.png'} alt="Enemy" />
     </div>
     `)
   }
