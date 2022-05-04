e5ef4a1294ce3756b687dead4ee1559dac94f08b
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-04 16:13:58 -0700

    populated createElement in Combatant.js

--- Battle/Combatant.js
+++ Battle/Combatant.js
@@ -1,13 +1,16 @@
 class Combatant {
   constructor(config, battle) {
     Object.keys(config).forEach(key => {
-      this[key] = config[key];
+      this[key] = config[key]; 204157.3
     })
     this.battle = battle;
   }
 
   createElement() {
-
+    this.hudElement = document.createElement("div");
+    this.hudElement.classList.add("Combatant");
+    this.hudElement.setAttribute("data-combatant", this.id);
+    this.hudElement.setAttribute("data-team", this.team)
   }
 
   init() {
