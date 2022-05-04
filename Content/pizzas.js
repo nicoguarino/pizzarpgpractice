036eb1a09d09117c4ab050813065ebc9d95c3dd1
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-04 15:43:45 -0700

    created three pizza objects

--- Content/pizzas.js
+++ Content/pizzas.js
@@ -5,3 +5,24 @@ window.PizzaTypes = {
   fungi: "fungi",
   chill: "chill"
 }
+
+window.Pizzas = {
+  "s001": {
+    name: "Slice Samurai",
+    type: PizzaTypes.spicy,
+    src: "/images/characters/pizas/s001.png",
+    icon: "/images/icons/spicy.png"
+  },
+  "v001": {
+    name: "Call Me Kale",
+    type: PizzaTypes.veggie,
+    src: "/images/characters/pizas/v001.png",
+    icon: "/images/icons/veggie.png",
+  },
+  "f001": {
+    name: "Portobello Express",
+    type: PizzaTypes.fungi,
+    src: "/images/characters/pizas/f001.png",
+    icon: "/images/icons/fungi.png",
+  }
+}
