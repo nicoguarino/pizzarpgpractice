bc0a3e89853806837db5cd9d68ffbcd6075c75cf
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-18 16:04:55 -0700

    found no errors in pizzas.js but replaced code anyways

--- Content/pizzas.js
+++ Content/pizzas.js
@@ -10,19 +10,19 @@ window.Pizzas = {
   "s001": {
     name: "Slice Samurai",
     type: PizzaTypes.spicy,
-    src: "/images/characters/pizas/s001.png",
-    icon: "/images/icons/spicy.png"
+    src: "/images/characters/pizzas/s001.png",
+    icon: "/images/icons/spicy.png",
   },
   "v001": {
     name: "Call Me Kale",
     type: PizzaTypes.veggie,
-    src: "/images/characters/pizas/v001.png",
+    src: "/images/characters/pizzas/v001.png",
     icon: "/images/icons/veggie.png",
   },
   "f001": {
     name: "Portobello Express",
     type: PizzaTypes.fungi,
-    src: "/images/characters/pizas/f001.png",
+    src: "/images/characters/pizzas/f001.png",
     icon: "/images/icons/fungi.png",
   }
 }
