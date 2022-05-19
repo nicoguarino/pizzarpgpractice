044f2f229dfb4a064a97792893f268a250fbfc87
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-19 15:54:44 -0700

    added name of basic attack, added success play out if it is a successful attack

--- Content/actions.js
+++ Content/actions.js
@@ -1,5 +1,9 @@
 window.Actions = {
   damage1: {
-    
+    name: "Whomp!",
+    success:[ 
+      {type: "textMessage", text: "Something Happened!"},
+      {type: "textMessage", text: "Something happened!"},
+    ]
   }
 }
