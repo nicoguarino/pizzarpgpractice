530d8b38c4c6b7e53767034ebd7969a07e11c6e9
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-05-25 15:29:44 -0700

    added test attack to all pizzas

--- Content/pizzas.js
+++ Content/pizzas.js
@@ -12,20 +12,20 @@ window.Pizzas = {
     type: PizzaTypes.spicy,
     src: "/images/characters/pizzas/s001.png",
     icon: "/images/icons/spicy.png",
-    actions: [ ]
+    actions: [ "damage1" ]
   },
   "v001": {
     name: "Call Me Kale",
     type: PizzaTypes.veggie,
     src: "/images/characters/pizzas/v001.png",
     icon: "/images/icons/veggie.png",
-    actions: [ ]
+    actions: [ "damage1" ]
   },
   "f001": {
     name: "Portobello Express",
     type: PizzaTypes.fungi,
     src: "/images/characters/pizzas/f001.png",
     icon: "/images/icons/fungi.png",
-    actions: [ ]
+    actions: [ "damage1" ]
   }
 }
